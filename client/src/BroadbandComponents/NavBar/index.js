583c3380e3bdf18b268aa3167e71fb835c1b8ac1
unknown <kimaniwachira03@gmail.com>
2022-01-19 06:26:38 +0300

    All the way to the NavBar

---
+++ client/src/BroadbandComponents/NavBar/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItems,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarElements";
+
+const NavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Broadband</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItems>
+              <NavLinks to="Home">Home</NavLinks>
+            </NavItems>
+            <NavItems>
+              <NavLinks to="Packages">Packages</NavLinks>
+            </NavItems>
+            <NavItems>
+              <NavLinks to="Discovey">Discover</NavLinks>
+            </NavItems>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to="SignIn">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ client/src/BroadbandComponents/SideBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon } from './SidebarElements'
+const SideBar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+
+            </Icon>
+            
+        </SidebarContainer>
+    )
+}
+
+export default SideBar
