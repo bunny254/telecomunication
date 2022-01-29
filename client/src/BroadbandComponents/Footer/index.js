a0548876e574417fbecbc7bbcf82621d8e891de5
unknown <kimaniwachira03@gmail.com>
2022-01-29 07:10:01 +0300

    All the way to the Footer

--- client/src/BroadbandComponents/Footer/index.js
+++ client/src/BroadbandComponents/Footer/index.js
@@ -1,47 +1,81 @@
-import React from 'react';
-import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink } from './FooterElements';
+import React from "react";
+import {FaInstagram, FaTwitter, FaLinkedin} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+  WebsiteRights
+} from "./FooterElements";
 
 const Footer = () => {
   return (
-  <FooterContainer>
+    <FooterContainer>
       <FooterWrap>
-          <FooterLinksContainer>
-              <FooterLinksWrapper>
-                  <FooterLinkItems>
-                      <FooterLinkTitle> About Us </FooterLinkTitle>
-                          <FooterLink to="/signin">Get Started </FooterLink>
-                          <FooterLink to="/signin">Our Clients </FooterLink>
-                          <FooterLink to="/signin">Careers </FooterLink>
-                          <FooterLink to="/signin">Terms of Service </FooterLink>
-                  </FooterLinkItems>
-                  <FooterLinkItems>
-                      <FooterLinkTitle> About Us </FooterLinkTitle>
-                          <FooterLink to="/signin">Get Started </FooterLink>
-                          <FooterLink to="/signin">Our Clients </FooterLink>
-                          <FooterLink to="/signin">Careers </FooterLink>
-                          <FooterLink to="/signin">Terms of Service </FooterLink>
-                  </FooterLinkItems>
-              </FooterLinksWrapper>
-              <FooterLinksWrapper>
-                  <FooterLinkItems>
-                      <FooterLinkTitle> About Us </FooterLinkTitle>
-                          <FooterLink to="/signin">Get Started </FooterLink>
-                          <FooterLink to="/signin">Our Clients </FooterLink>
-                          <FooterLink to="/signin">Careers </FooterLink>
-                          <FooterLink to="/signin">Terms of Service </FooterLink>
-                  </FooterLinkItems>
-                  <FooterLinkItems>
-                      <FooterLinkTitle> About Us </FooterLinkTitle>
-                          <FooterLink to="/signin">Get Started </FooterLink>
-                          <FooterLink to="/signin">Our Clients </FooterLink>
-                          <FooterLink to="/signin">Careers </FooterLink>
-                          <FooterLink to="/signin">Terms of Service </FooterLink>
-                  </FooterLinkItems>
-              </FooterLinksWrapper>
-          </FooterLinksContainer>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us </FooterLinkTitle>
+              <FooterLink to="/signin">Get Started </FooterLink>
+              <FooterLink to="/signin">Our Clients </FooterLink>
+              <FooterLink to="/signin">Careers </FooterLink>
+              <FooterLink to="/signin">Terms of Service </FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us </FooterLinkTitle>
+              <FooterLink to="/signin">Get Started </FooterLink>
+              <FooterLink to="/signin">Our Clients </FooterLink>
+              <FooterLink to="/signin">Careers </FooterLink>
+              <FooterLink to="/signin">Terms of Service </FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us </FooterLinkTitle>
+              <FooterLink to="/signin">Get Started </FooterLink>
+              <FooterLink to="/signin">Our Clients </FooterLink>
+              <FooterLink to="/signin">Careers </FooterLink>
+              <FooterLink to="/signin">Terms of Service </FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us </FooterLinkTitle>
+              <FooterLink to="/signin">Get Started </FooterLink>
+              <FooterLink to="/signin">Our Clients </FooterLink>
+              <FooterLink to="/signin">Careers </FooterLink>
+              <FooterLink to="/signin">Terms of Service </FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Kim</SocialLogo>
+            <WebsiteRights>
+              Made With ❤️ By Simon Wachira © {new Date().getFullYear()}{" "}
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
-</FooterContainer>
- );
+    </FooterContainer>
+  );
 };
 
 export default Footer;
